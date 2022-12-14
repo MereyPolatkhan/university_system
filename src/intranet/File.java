01bfc1eeef567507b4a3f0eca78a0c19c2d2bb07
aknur <95418973+aknurkappar@users.noreply.github.com>
2022-12-14 19:56:57 +0600

    Update File.java

--- src/intranet/File.java
+++ src/intranet/File.java
@@ -1,74 +1,9 @@
 package intranet;
 
-
-/**
-* @generated
-*/
 public class File {
     
-    /**
-    * @generated
-    */
-    private String name;
-    
-    /**
-    * @generated
-    */
-    private  Data;
-    
-    
-    /**
-    * @generated
-    */
-    private Teacher teacher;
-    
-    
+	public String name;
 
-    /**
-    * @generated
-    */
-    public String getName() {
-        return this.name;
-    }
-    
-    /**
-    * @generated
-    */
-    public String setName(String name) {
-        this.name = name;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public  getData() {
-        return this.Data;
-    }
-    
-    /**
-    * @generated
-    */
-    public  setData(invalid Data) {
-        this.Data = Data;
-    }
-    
-    
-    
-    /**
-    * @generated
-    */
-    public Teacher getTeacher() {
-        return this.teacher;
-    }
-    
-    /**
-    * @generated
-    */
-    public Teacher setTeacher(Teacher teacher) {
-        this.teacher = teacher;
-    }
-    
-    
+	public String Data; // type 
     
 }
