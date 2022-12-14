aaa561376c6e71d482fbd90ba80cff6bd086a4f0
aknur <95418973+aknurkappar@users.noreply.github.com>
2022-12-14 22:47:57 +0600

    Update Student.java

--- src/intranet/Student.java
+++ src/intranet/Student.java
@@ -1,463 +1,99 @@
 package intranet;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-/**
-* @generated
-*/
 public class Student extends UserDecorator {
-    
-    /**
-    * @generated
-    */
-    private Vector<Student> students;
-    
-    /**
-    * @generated
-    */
-    private HashMap<Course, Mark> currentCoursesMarks;
-    
-    /**
-    * @generated
-    */
-    private Period currentPeriod;
-    
-    /**
-    * @generated
-    */
-    private Transcript transcript;
-    
-    /**
-    * @generated
-    */
-    private Organization organization;
-    
-    /**
-    * @generated
-    */
-    private StudentLevel level;
-    
-    /**
-    * @generated
-    */
-    private int year;
-    
-    /**
-    * @generated
-    */
-    private Department department;
-    
-    /**
-    * @generated
-    */
-    private Speciality studentSpeciality;
-    
-    /**
-    * @generated
-    */
+	
+	private HashMap<Course, Mark> currentCoursesMarks;
+    public Period period;
+    private Transcript transcrcipt;
+    public Organization organization;
+    public StudentLevel level;
+    public int year;
+    public Department department;
+    public Speciality studentSpeciality;
     private int scholarship;
-    
-    /**
-    * @generated
-    */
     private Schedule schedule;
+    public double generalGPA;
     
-    
-    /**
-    * @generated
-    */
-    private DiplomaProjects diplomaProjects;
-    
-    /**
-    * @generated
-    */
-    private Transcript transcript2;
-    
-    /**
-    * @generated
-    */
-    private Database database;
-    
-    /**
-    * @generated
-    */
-    private Schedule schedule2;
-    
-    /**
-    * @generated
-    */
-    private Organization organization;
-    
-    /**
-    * @generated
-    */
-    private Department department2;
-    
-    /**
-    * @generated
-    */
-    private Mark mark;
-    
-    /**
-    * @generated
-    */
-    private Period period;
-    
-    
-
-    /**
-    * @generated
-    */
-    public Vector<Student> getStudents() {
-        return this.students;
+    public int getScholarship() {
+    	return scholarship;
+    }
+    public Schedule getSchedule() {
+    	return schedule;
     }
     
-    /**
-    * @generated
-    */
-    public Vector<Student> setStudents(Vector<Student> students) {
-        this.students = students;
-    }
+    public void setMark(Course course, Mark mark) {
+    	currentCoursesMarks.put(course, mark);
+    } 
     
+    public Student(User user) {
+		super(user);
+	}
     
-    /**
-    * @generated
-    */
-    private Map<Course, Mark> getCurrentCoursesMarks() {
-        return this.currentCoursesMarks;
-    }
-    
-    /**
-    * @generated
-    */
-    private Map<Course, Mark> setCurrentCoursesMarks(Map<Course, Mark> currentCoursesMarks) {
-        this.currentCoursesMarks = currentCoursesMarks;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Period getCurrentPeriod() {
-        return this.currentPeriod;
-    }
-    
-    /**
-    * @generated
-    */
-    public Period setCurrentPeriod(Period currentPeriod) {
-        this.currentPeriod = currentPeriod;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    private Transcript getTranscript() {
-        return this.transcript;
-    }
-    
-    /**
-    * @generated
-    */
-    private Transcript setTranscript(Transcript transcript) {
-        this.transcript = transcript;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Organization getOrganization() {
-        return this.organization;
-    }
-    
-    /**
-    * @generated
-    */
-    public Organization setOrganization(Organization organization) {
-        this.organization = organization;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public StudentLevel getLevel() {
-        return this.level;
-    }
-    
-    /**
-    * @generated
-    */
-    public StudentLevel setLevel(StudentLevel level) {
-        this.level = level;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public int getYear() {
-        return this.year;
-    }
-    
-    /**
-    * @generated
-    */
-    public int setYear(Integer year) {
-        this.year = year;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Department getDepartment() {
-        return this.department;
-    }
-    
-    /**
-    * @generated
-    */
-    public Department setDepartment(Department department) {
-        this.department = department;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Speciality getStudentSpeciality() {
-        return this.studentSpeciality;
-    }
-    
-    /**
-    * @generated
-    */
-    public Speciality setStudentSpeciality(Speciality studentSpeciality) {
-        this.studentSpeciality = studentSpeciality;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    private int getScholarship() {
-        return this.scholarship;
-    }
-    
-    /**
-    * @generated
-    */
-    private int setScholarship(Integer scholarship) {
-        this.scholarship = scholarship;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    private Schedule getSchedule() {
-        return this.schedule;
-    }
-    
-    /**
-    * @generated
-    */
-    private Schedule setSchedule(Schedule schedule) {
-        this.schedule = schedule;
-    }
-    
-    
-    
-    /**
-    * @generated
-    */
-    public Mark getMark() {
-        return this.mark;
-    }
-    
-    /**
-    * @generated
-    */
-    public Mark setMark(Mark mark) {
-        this.mark = mark;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Schedule getSchedule2() {
-        return this.schedule2;
-    }
-    
-    /**
-    * @generated
-    */
-    public Schedule setSchedule2(Schedule schedule2) {
-        this.schedule2 = schedule2;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Period getPeriod() {
-        return this.period;
-    }
-    
-    /**
-    * @generated
-    */
-    public Period setPeriod(Period period) {
-        this.period = period;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Transcript getTranscript2() {
-        return this.transcript2;
-    }
-    
-    /**
-    * @generated
-    */
-    public Transcript setTranscript2(Transcript transcript2) {
-        this.transcript2 = transcript2;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Database getDatabase() {
-        return this.database;
-    }
-    
-    /**
-    * @generated
-    */
-    public Database setDatabase(Database database) {
-        this.database = database;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Organization getOrganization() {
-        return this.organization;
-    }
-    
-    /**
-    * @generated
-    */
-    public Organization setOrganization(Organization organization) {
-        this.organization = organization;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public DiplomaProjects getDiplomaProjects() {
-        return this.diplomaProjects;
-    }
-    
-    /**
-    * @generated
-    */
-    public DiplomaProjects setDiplomaProjects(DiplomaProjects diplomaProjects) {
-        this.diplomaProjects = diplomaProjects;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Department getDepartment2() {
-        return this.department2;
-    }
-    
-    /**
-    * @generated
-    */
-    public Department setDepartment2(Department department2) {
-        this.department2 = department2;
-    }
-    
-    
-    
+    public Student(User user, Department department, Speciality studentSpeciality, StudentLevel level, int year) {
+    	super(user);
+    	this.department = department;
+    	this.studentSpeciality = studentSpeciality;
+    	this.level = level;
+    	this.year = year;
+    } 
 
     //                          Operations                                  
-    
-    /**
-    * @generated
-    */
-    public boolean registerCourse() {
-        //TODO
-        return false;
+
+    public boolean registerCourse(Course course) {
+    	try {
+    		String definition = "Course name " + course.getCourseName();
+        	new RequestFromStudent("Register for course", definition, this);
+        	return true;
+    	} catch (Exception e) {
+    		throw new RuntimeException("Exception in putting mark");
+    	}
+    	  // Student creates request and add to requests Vector (send to Manager)
+    	  // Request constructor / Request(String header, String definition, BasicUser sender)
+    	  // But now we cannot convert Student to BasicUser, so I create subclass RequestFromStudent
     }
     
-    /**
-    * @generated
-    */
-    public Vector<Teacher> getTeacherInfo() {
-        //TODO
-        return null;
+    public Vector<Teacher> getTeacherInfo(Course course) {
+    	return course.getTeachers();
+    }
+
+    public HashMap<Course, Mark> viewMarks() {
+        return currentCoursesMarks;
     }
     
-    /**
-    * @generated
-    */
-    private Vector<Mark> viewMarks() {
+    public Transcript viewTranscript() {
         //TODO
-        return null;
+        return transcrcipt;
     }
-    
-    /**
-    * @generated
-    */
-    private Transcript viewTranscript() {
-        //TODO
-        return null;
+  
+	public Vector<Course> viewCourses() {
+    	  return Datebase.courses.stream()
+    		    	.filter(c->c.equals(currentCoursesMarks.keySet()))
+    		    	.collect(Collectors.toCollection(Vector::new));
     }
-    
-    /**
-    * @generated
-    */
-    public Vector<Course> viewCourses() {
-        //TODO
-        return null;
+
+    public boolean rateTeacher(Teacher teacher, Double rate) {
+    	try {
+    		if(this.viewCourses().stream()
+        			.anyMatch(c->c.getTeachers().contains(teacher)))
+    			teacher.setTeacherRate(rate);
+        	return true;
+    	} catch (Exception e) {
+    		throw new RuntimeException("Exception in putting mark");
+    	}
     }
-    
-    /**
-    * @generated
-    */
-    private boolean rateTeacher() {
-        //TODO
-        return false;
+  
+    public Transcript getTranscript() {
+		return transcrcipt;
     }
-    
-    /**
-    * @generated
-    */
-    private Transcript getTranscript() {
-        //TODO
-        return null;
+
+    public boolean writeDiplomaProject(DiplomaProjects d) {
+    	 DiplomaProjects.projects.add(d);
+    	 return true;
     }
-    
-    /**
-    * @generated
-    */
-    private boolean writeDiplomaProject() {
-        //TODO
-        return false;
-    }
-    
-    
 }
