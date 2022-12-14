25ab29e3f3bff452e0d26abd65c600fdc39a5ef8
aknur <95418973+aknurkappar@users.noreply.github.com>
2022-12-14 19:51:11 +0600

    Update DiplomaProjects.java

--- src/intranet/DiplomaProjects.java
+++ src/intranet/DiplomaProjects.java
@@ -1,58 +1,14 @@
 package intranet;
 
+import java.util.Vector;
 
-/**
-* @generated
-*/
 public class DiplomaProjects {
-    
-    /**
-    * @generated
-    */
-    private Vector<DiplomaProjects> projects;
-    
-    
-    /**
-    * @generated
-    */
-    private Student student;
-    
-    
-
-    /**
-    * @generated
-    */
-    public Vector<DiplomaProjects> getProjects() {
-        return this.projects;
-    }
-    
-    /**
-    * @generated
-    */
-    public Vector<DiplomaProjects> setProjects(Vector<DiplomaProjects> projects) {
-        this.projects = projects;
-    }
-    
-    
-    
-    /**
-    * @generated
-    */
-    public Student getStudent() {
-        return this.student;
-    }
-    
-    /**
-    * @generated
-    */
-    public Student setStudent(Student student) {
-        this.student = student;
-    }
-    
-    
+	
+	public static Vector<DiplomaProjects> projects;
     
 
     //                          Operations                                  
     
+	// doProject()
     
 }
