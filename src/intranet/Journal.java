75451b5df3bf57719c129bc09c4385d3ecb9981e
aknur <95418973+aknurkappar@users.noreply.github.com>
2022-12-15 09:35:11 +0600

    Update Journal.java

--- src/intranet/Journal.java
+++ src/intranet/Journal.java
@@ -7,8 +7,16 @@ public class Journal{
     public Period period;
     HashMap<Course, Mark> courses;
     
+    public Journal() {}
+    
+    public Journal(Period period, HashMap<Course, Mark> courses ) {
+    	this.period = period;
+    	this.courses = courses;
+    }
+    
     public Double showTotal(Course course) {
     	return courses.get(course).digitGrade;
     }
 	
 }
+ 
