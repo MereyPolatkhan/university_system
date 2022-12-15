4aae554047a4536676169ef46123076af09f656a
aknur <95418973+aknurkappar@users.noreply.github.com>
2022-12-15 09:37:18 +0600

    Update Transcript.java

--- src/intranet/Transcript.java
+++ src/intranet/Transcript.java
@@ -7,6 +7,15 @@ public class Transcript {
 	public int numberOfFailedCourses;
 	public Vector<Journal> journals;
 	
+	public Transcript(){
+		numberOfFailedCourses = 0;
+	}
+	
+	public Transcript(Vector<Journal> journals){
+		this();
+		this.journals = journals;
+	}
+	
 	{
 	if(numberOfFailedCourses >= 3)
 		leaveUniversity();
@@ -15,5 +24,4 @@ public class Transcript {
 	private void leaveUniversity() {
 		//removeUser?
 	}
-	
 }
