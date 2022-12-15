04e2b0c1273a2ab0ba7a122fad9eeda27786d1c2
aknur <95418973+aknurkappar@users.noreply.github.com>
2022-12-15 09:20:12 +0600

    Update Organization.java

--- src/intranet/Organization.java
+++ src/intranet/Organization.java
@@ -1,174 +1,19 @@
 package intranet;
+import java.util.Vector;
 
-
-/**
-* @generated
-*/
 public class Organization {
     
-    /**
-    * @generated
-    */
-    private Vector<Organization> organizations;
+	public Vector<Organization> organizations;
     
-    /**
-    * @generated
-    */
-    private String name ;
+	public String name ;
     
-    /**
-    * @generated
-    */
-    private Student head;
+	public Student head;
     
-    /**
-    * @generated
-    */
-    private Vector<User> members;
+	public Vector<User> members;
     
-    /**
-    * @generated
-    */
-    private String instagramAccLink;
-    
-    /**
-    * @generated
-    */
-    private String telegramAccLink;
-    
-    
-    /**
-    * @generated
-    */
-    private Database database;
-    
-    /**
-    * @generated
-    */
-    private Student student;
-    
-    
+	public String instagramAccLink;
 
-    /**
-    * @generated
-    */
-    public Vector<Organization> getOrganizations() {
-        return this.organizations;
-    }
-    
-    /**
-    * @generated
-    */
-    public Vector<Organization> setOrganizations(Vector<Organization> organizations) {
-        this.organizations = organizations;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public String getName () {
-        return this.name ;
-    }
-    
-    /**
-    * @generated
-    */
-    public String setName (String name ) {
-        this.name  = name ;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Student getHead() {
-        return this.head;
-    }
-    
-    /**
-    * @generated
-    */
-    public Student setHead(Student head) {
-        this.head = head;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Vector<User> getMembers() {
-        return this.members;
-    }
-    
-    /**
-    * @generated
-    */
-    public Vector<User> setMembers(Vector<User>  members) {
-        this.members = members;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public String getInstagramAccLink() {
-        return this.instagramAccLink;
-    }
-    
-    /**
-    * @generated
-    */
-    public String setInstagramAccLink(String instagramAccLink) {
-        this.instagramAccLink = instagramAccLink;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public String getTelegramAccLink() {
-        return this.telegramAccLink;
-    }
-    
-    /**
-    * @generated
-    */
-    public String setTelegramAccLink(String telegramAccLink) {
-        this.telegramAccLink = telegramAccLink;
-    }
-    
-    
-    
-    /**
-    * @generated
-    */
-    public Student getStudent() {
-        return this.student;
-    }
-    
-    /**
-    * @generated
-    */
-    public Student setStudent(Student student) {
-        this.student = student;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Database getDatabase() {
-        return this.database;
-    }
-    
-    /**
-    * @generated
-    */
-    public Database setDatabase(Database database) {
-        this.database = database;
-    }
-    
-    
-    
+	public String telegramAccLink;
+	
+	//                                 Operations
 }
