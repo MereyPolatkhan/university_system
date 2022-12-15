0103741cc24c4b3e0c27778050eb28677000eb63
aknur <95418973+aknurkappar@users.noreply.github.com>
2022-12-15 09:18:43 +0600

    Update Period.java

--- src/intranet/Period.java
+++ src/intranet/Period.java
@@ -1,94 +1,34 @@
 package intranet;
 
-
-/**
-* @generated
-*/
 public class Period {
-    
-    /**
-    * @generated
-    */
+	
     private int beginYear;
-    
-    /**
-    * @generated
-    */
-    private int endYear;
-    
-    /**
-    * @generated
-    */
-    private Semester semester;
-    
-    
-    /**
-    * @generated
-    */
-    private Student student;
-    
-    
 
-    /**
-    * @generated
-    */
+    private int endYear;
+
+    private Semester semester;
+
     public int getBeginYear() {
         return this.beginYear;
     }
-    
-    /**
-    * @generated
-    */
-    public int setBeginYear(Integer beginYear) {
+
+    public void setBeginYear(Integer beginYear) {
         this.beginYear = beginYear;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     public int getEndYear() {
         return this.endYear;
     }
-    
-    /**
-    * @generated
-    */
-    public int setEndYear(Integer endYear) {
+ 
+    public void setEndYear(Integer endYear) {
         this.endYear = endYear;
     }
     
-    
-    /**
-    * @generated
-    */
     public Semester getSemester() {
         return this.semester;
     }
-    
-    /**
-    * @generated
-    */
-    public Semester setSemester(Semester semester) {
+
+    public void setSemester(Semester semester) {
         this.semester = semester;
     }
-    
-    
-    
-    /**
-    * @generated
-    */
-    public Student getStudent() {
-        return this.student;
-    }
-    
-    /**
-    * @generated
-    */
-    public Student setStudent(Student student) {
-        this.student = student;
-    }
-    
-    
-    
 }
