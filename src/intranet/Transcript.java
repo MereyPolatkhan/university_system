bb10619fa08335f8355cec93a09afd53e15fb3c3
aknur <95418973+aknurkappar@users.noreply.github.com>
2022-12-15 09:16:50 +0600

    Update Transcript.java

--- src/intranet/Transcript.java
+++ src/intranet/Transcript.java
@@ -2,94 +2,18 @@ package intranet;
 
 import java.util.Vector;
 
-/**
-* @generated
-*/
 public class Transcript {
-    
-    /**
-    * @generated
-    */
-    private Vector<Journal> journals;
-    
-    
-    /**
-    * @generated
-    */
-    private Database database;
-    
-    /**
-    * @generated
-    */
-    private Student student;
-    
-    /**
-    * @generated
-    */
-    private Journal journal;
-    
-    
+	
+	public int numberOfFailedCourses;
+	public Vector<Journal> journals;
+	
+	{
+	if(numberOfFailedCourses >= 3)
+		leaveUniversity();
+	}
 
-    /**
-    * @generated
-    */
-    public Vector<Journal> getJournals() {
-        return this.journals;
-    }
-    
-    /**
-    * @generated
-    */
-    public Vector<Journal> setJournals(Vector<Journal> journals) {
-        this.journals = journals;
-    }
-    
-    
-    
-    /**
-    * @generated
-    */
-    public Journal getJournal() {
-        return this.journal;
-    }
-    
-    /**
-    * @generated
-    */
-    public Journal setJournal(Journal journal) {
-        this.journal = journal;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Student getStudent() {
-        return this.student;
-    }
-    
-    /**
-    * @generated
-    */
-    public Student setStudent(Student student) {
-        this.student = student;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Database getDatabase() {
-        return this.database;
-    }
-    
-    /**
-    * @generated
-    */
-    public Database setDatabase(Database database) {
-        this.database = database;
-    }
-    
-    
-    
+	private void leaveUniversity() {
+		//removeUser?
+	}
+	
 }
